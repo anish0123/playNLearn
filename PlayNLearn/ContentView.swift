809f1fd73741d64091a136b169dc114117d9744b
Ritesh Ghimire <argier96@gmail.com>
2023-04-03 00:50:58 +0300

    Initial Commit

---
+++ PlayNLearn/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PlayNLearn
+//
+//  Created by Ritesh Ghimire on 3.4.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PlayNLearn/PlayNLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayNLearnApp.swift
+//  PlayNLearn
+//
+//  Created by Ritesh Ghimire on 3.4.2023.
+//
+
+import SwiftUI
+
+@main
+struct PlayNLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
